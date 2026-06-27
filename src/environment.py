87ec521dc46b2f041cc29dfb067552c7801ef635
DontDDoS <git@dontddos.me>
2026-06-27 16:47:37 +0100

    Capitalisation to represent constance

--- src/environment.py
+++ src/environment.py
@@ -4,8 +4,8 @@ def get_env(name):
     return os.environ.get(name)
 
 # Postgres environment variables
-postgres_user       = get_env("POSTGRES_USER")
-postgres_password   = get_env("POSTGRES_PASSWORD")
-postgres_db         = get_env("POSTGRES_DB")
-postgres_host       = get_env("POSTGRES_HOST")
-postgres_port       = int(get_env("POSTGRES_PORT"))
+POSTGRES_USER       = get_env("POSTGRES_USER")
+POSTGRES_PASSWORD   = get_env("POSTGRES_PASSWORD")
+POSTGRES_DB         = get_env("POSTGRES_DB")
+POSTGRES_HOST       = get_env("POSTGRES_HOST")
+POSTGRES_PORT       = int(get_env("POSTGRES_PORT"))
