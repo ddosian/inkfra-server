556a5ec30acd0ee50463e0fa9277aa8da96d018b
DontDDoS <git@dontddos.me>
2026-06-27 16:42:57 +0100

    Created get_env function

--- src/environment.py
+++ src/environment.py
@@ -1,8 +1,11 @@
 import os
 
+def get_env(name):
+    return os.environ.get(name)
+
 # Postgres environment variables
-postgres_user       = os.environ.get("POSTGRES_USER")
-postgres_password   = os.environ.get("POSTGRES_PASSWORD")
-postgres_db         = os.environ.get("POSTGRES_DB")
-postgres_host       = os.environ.get("POSTGRES_HOST")
-postgres_port       = int(os.environ.get("POSTGRES_PORT"))
+postgres_user       = get_env("POSTGRES_USER")
+postgres_password   = get_env("POSTGRES_PASSWORD")
+postgres_db         = get_env("POSTGRES_DB")
+postgres_host       = get_env("POSTGRES_HOST")
+postgres_port       = int(get_env("POSTGRES_PORT"))
